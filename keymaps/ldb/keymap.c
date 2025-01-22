cc384a2d6274e4cb200a561244cc12ede3633479
LdBeth <andpuke@foxmail.com>
2025-01-21 18:16:51 -0600

    first commit

---
+++ keymaps/ldb/keymap.c
@@ -0,0 +1,92 @@
+/* Copyright 2025 LdBeth
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+// Defines names for use in layer keycodes and the keymap
+enum layer_names {
+  _BASE,
+  _FN
+};
+
+// Defines the keycodes used by our macros in process_record_user
+enum custom_keycodes {
+    ALL_U = SAFE_RANGE,
+    U_ARG, J_UST, I_TS, G_GN,
+    TOGG_RA
+};
+
+#define RSTGPU LGUI(LCTL(LSFT(KC_B)))
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* Base */
+    [_BASE] = LAYOUT_fullsize_ansi(
+        KC_ESC,        KC_F1, KC_F2, KC_F3, KC_F4,    KC_F5, KC_F6, KC_F7, KC_F8,     KC_F9, KC_F10, KC_F11, KC_F12,    KC_PSCR, KC_SCRL, KC_PAUS,
+        KC_GRV, KC_1,  KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,  KC_0,  KC_MINS, KC_EQL,      KC_BSPC,    KC_INS,  KC_HOME, KC_PGUP,    KC_NUM, KC_PSLS, KC_PAST, KC_PMNS,
+        KC_TAB,    KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,  KC_LBRC, KC_RBRC,  KC_BSLS,    KC_DEL,  KC_END,  KC_PGDN,    KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
+        KC_CAPS,      KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,  KC_L,  KC_SCLN, KC_QUOT,       KC_ENT,                                  KC_P4,   KC_P5,   KC_P6,         
+        KC_LSFT,         KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM, KC_DOT, KC_SLSH,         KC_RSFT,             KC_UP,               KC_P1,   KC_P2,   KC_P3,   KC_PENT,
+        KC_LCTL,  KC_LGUI, KC_LALT,                KC_SPC,                       KC_RGUI, KC_RALT, MO(_FN), KC_RCTL,    KC_LEFT, KC_DOWN, KC_RGHT,         KC_P0,       KC_PDOT
+                         ),
+    [_FN] = LAYOUT_fullsize_ansi(
+        KC_TRNS,    KC_BRID, KC_BRIU, KC_TRNS, KC_TRNS,  KC_MSTP, KC_MPRV, KC_MPLY, KC_MNXT,  KC_WBAK, KC_WFWD, KC_TRNS, KC_TRNS,   KC_F13, KC_F14, KC_F15,
+        KC_ESC,   DM_PLY1,DM_PLY2,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_MUTE,KC_VOLD,KC_VOLU,KC_EJCT,    KC_F16, KC_F17, KC_F18,     KC_F22,   KC_F23,   KC_F24,  KC_TRNS,
+        KC_TRNS,    KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  U_ARG,  KC_U,  I_TS,  KC_O,  KC_P,  KC_TRNS, KC_TRNS,  KC_TRNS,        KC_F19, KC_F20, KC_F21,     KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,
+        QK_LOCK,      ALL_U,  KC_S,  KC_D,  KC_F,  G_GN,  KC_H,  J_UST,  KC_K,  KC_L,  KC_TRNS, KC_TRNS,       KC_TRNS,                                   KC_TRNS, KC_TRNS,  KC_TRNS,         
+        KC_TRNS,         KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_TRNS,KC_TRNS, KC_TRNS,         KC_TRNS,                   KC_TRNS,           DM_REC1, DM_REC2,  DM_RSTP,  KC_TRNS,
+        KC_TRNS,  RSTGPU, KC_TRNS,                KC_TRNS,                       KC_MENU, TOGG_RA, KC_TRNS, NK_TOGG,          KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS,      KC_TRNS
+    )
+};
+
+#define DEF_MACRO_KEY(code, str) case code: \
+    if (record->event.pressed) { \
+      SEND_STRING(str); \
+    } else { } break
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    DEF_MACRO_KEY(ALL_U,"allyourbasearebelongtous");
+    DEF_MACRO_KEY(U_ARG,"yourargumentisinvalid");
+    DEF_MACRO_KEY(J_UST,"justaccordingtokeikaku");
+    DEF_MACRO_KEY(G_GN,"goodnightsweetprince");
+    DEF_MACRO_KEY(I_TS,"itsmorelikelythanyouthink");
+  case TOGG_RA:
+    if (record->event.pressed) {
+      keymap_config.raw = eeconfig_read_keymap();
+      keymap_config.swap_ralt_rgui = !keymap_config.swap_ralt_rgui;
+      keymap_config.swap_control_capslock = keymap_config.swap_ralt_rgui;
+      eeconfig_update_keymap(keymap_config.raw);
+      clear_keyboard();
+      return false;
+    }
+    break;
+  }
+  return true;
+}
+
+/*
+void matrix_init_user(void) {
+
+}
+
+void matrix_scan_user(void) {
+
+}
+
+bool led_update_user(led_t led_state) {
+    return true;
+}
+*/
+
---
+++ readme.md
@@ -0,0 +1,21 @@
+# Beamspring 104 Round 1
+
+
+*A short description of the keyboard/project*
+
+* Keyboard Maintainer: [LdBeth](https://github.com/ldbeth)
+* Hardware Supported: Model F Labs Beamspring Round 1 xwhatsit
+* Hardware Availability: https://www.modelfkeyboards.com/store/
+
+Make example for this keyboard (after setting up your build environment):
+
+    make beamspringr1:ldb
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
+
+## Bootloader
+
+Enter the bootloader in 2 ways:
+
+* **Physical reset button**: Short the PROG pins on the PCB
+* **Keycode in layout**: Left Shift+Right Shift+B (QMK Command Keycode)
