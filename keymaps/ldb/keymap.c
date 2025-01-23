16a567483d27c1fa5e55df1c21e9a00ef8786458
LdBeth <andpuke@foxmail.com>
2025-01-23 03:49:09 -0600

    typo!

--- keymaps/ldb/keymap.c
+++ keymaps/ldb/keymap.c
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT,         KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM, KC_DOT, KC_SLSH,         KC_RSFT,             KC_UP,               KC_P1,   KC_P2,   KC_P3,   KC_PENT,
         KC_LCTL,  KC_LGUI, KC_LALT,                KC_SPC,                       KC_RGUI, KC_RALT, MO(_XFN),KC_RCTL,    KC_LEFT, KC_DOWN, KC_RGHT,         KC_P0,       KC_PDOT
                                    ),
-    [_FN] = LAYOUT_fullsize_ansi(
+    [_XFN] = LAYOUT_fullsize_ansi(
         KC_TRNS,    KC_F1, KC_F2, KC_F3, KC_F4,    KC_F5, KC_F6, KC_F7, KC_F8,     KC_F9, KC_F10, KC_F11, KC_F12,                        KC_F16, KC_F17, KC_F18,
 
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_EJCT,    KC_TRNS,  KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
